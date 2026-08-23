e174214ba19e1d988282203bed3e763e059d3bc0
Aljam <aljam@live.ca>
2026-08-23 16:34:39 -0400

    Add Jellyfin user to media group
    Added jellyfin user to media group and set group for Jellyfin service.

--- modules/features/jellyfin.nix
+++ modules/features/jellyfin.nix
@@ -3,7 +3,11 @@
 {
   services.jellyfin = {
     enable = true;
+    group = "media";
   };
 
   networking.proxyBackendPorts = [ 8096 ];
+
+  # Add jellyfin user to media group
+  users.users.jellyfin.extraGroups = [ "media" ];
 }

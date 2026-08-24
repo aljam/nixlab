63c102bb39d404a79d71020e7275691d1553c1c0
Aljam <aljam@live.ca>
2026-08-24 03:50:46 -0400

    Refactor Prometheus server Nix configuration
    Refactor Prometheus server configuration for clarity.

--- modules/features/prometheus-server.nix
+++ modules/features/prometheus-server.nix
@@ -1,39 +1,43 @@
-services.prometheus = {
-  enable = true;
-  port = 9090;
-  listenAddress = config.networking.servicesBindAddress;
+{ config, lib, pkgs, ... }:
 
-  globalConfig = {
-    scrape_interval = "15s";
-    evaluation_interval = "15s";
+{
+  services.prometheus = {
+    enable = true;
+    port = 9090;
+    listenAddress = config.networking.servicesBindAddress;
+  
+    globalConfig = {
+      scrape_interval = "15s";
+      evaluation_interval = "15s";
+    };
+  
+    scrapeConfigs = [
+      {
+        job_name = "prometheus";
+        static_configs = [
+          {
+            targets = [ "127.0.0.1:9090" ];
+          }
+        ];
+      }
+  
+      {
+        job_name = "node";
+        static_configs = [
+          {
+            targets = [ "${config.networking.fleet.r730.ip}:9100" ];
+            labels = { host = "r730"; };
+          }
+          {
+            targets = [ "${config.networking.fleet.r730xd.ip}:9100" ];
+            labels = { host = "r730xd"; };
+          }
+          {
+            targets = [ "${config.networking.fleet.r820.ip}:9100" ];
+            labels = { host = "r820"; };
+          }
+        ];
+      }
+    ];
   };
-
-  scrapeConfigs = [
-    {
-      job_name = "prometheus";
-      static_configs = [
-        {
-          targets = [ "127.0.0.1:9090" ];
-        }
-      ];
-    }
-
-    {
-      job_name = "node";
-      static_configs = [
-        {
-          targets = [ "${config.networking.fleet.r730.ip}:9100" ];
-          labels = { host = "r730"; };
-        }
-        {
-          targets = [ "${config.networking.fleet.r730xd.ip}:9100" ];
-          labels = { host = "r730xd"; };
-        }
-        {
-          targets = [ "${config.networking.fleet.r820.ip}:9100" ];
-          labels = { host = "r820"; };
-        }
-      ];
-    }
-  ];
-};
+}

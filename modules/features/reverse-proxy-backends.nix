00405088554224a70f9b4d6d6118538e92495e4a
Aljam <aljam@live.ca>
2026-08-24 04:32:14 -0400

    Refactor firewall rules for backend and Prometheus access

--- modules/features/reverse-proxy-backends.nix
+++ modules/features/reverse-proxy-backends.nix
@@ -2,26 +2,17 @@
 
 let
   backendPorts = config.networking.proxyBackendPorts;
-  prometheusScrapeSources = [
-    config.networking.fleet.r730.ip
-    config.networking.fleet.r730xd.ip
-    config.networking.fleet.r820.ip
-  ];
 in
 {
-  networking.firewall.extraInputRules = lib.concatStringsSep "\n" (
-    lib.optional (backendPorts != []) ''
+  networking.firewall.extraInputRules =
+    (lib.optionalString (backendPorts != []) ''
       ip saddr ${config.networking.fleet.proxy.ip} tcp dport {
         ${lib.concatStringsSep ", " (map toString backendPorts)}
       } accept comment "HAProxy backend access"
-    ''
-
-    [
-      ''
-        ip saddr {
-          ${lib.concatStringsSep ", " prometheusScrapeSources}
-        } tcp dport 9100 accept comment "Prometheus node exporter scrape"
-      ''
-    ]
-  );
+    '')
+    + ''
+      ip saddr ${config.networking.fleet.r730xd.ip}
+      tcp dport 9100
+      accept comment "Prometheus node exporter scrape"
+    '';
 }

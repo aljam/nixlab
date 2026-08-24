c56daf04d653874b0650be3381c148ec2985435e
Aljam <aljam@live.ca>
2026-08-24 04:29:18 -0400

    Refactor firewall rules and add Prometheus sources
    Refactor firewall rules for proxy backends and add Prometheus scrape sources.

--- modules/features/reverse-proxy-backends.nix
+++ modules/features/reverse-proxy-backends.nix
@@ -1,7 +1,27 @@
 { config, lib, ... }:
 
+let
+  backendPorts = config.networking.proxyBackendPorts;
+  prometheusScrapeSources = [
+    config.networking.fleet.r730.ip
+    config.networking.fleet.r730xd.ip
+    config.networking.fleet.r820.ip
+  ];
+in
 {
-  networking.firewall.extraInputRules = lib.mkIf (config.networking.proxyBackendPorts != []) ''
-    ip saddr ${config.networking.fleet.proxy.ip} tcp dport { ${lib.concatStringsSep ", " (map toString config.networking.proxyBackendPorts)} } accept comment "HAProxy backend access"
-  '';
+  networking.firewall.extraInputRules = lib.concatStringsSep "\n" (
+    lib.optional (backendPorts != []) ''
+      ip saddr ${config.networking.fleet.proxy.ip} tcp dport {
+        ${lib.concatStringsSep ", " (map toString backendPorts)}
+      } accept comment "HAProxy backend access"
+    ''
+
+    [
+      ''
+        ip saddr {
+          ${lib.concatStringsSep ", " prometheusScrapeSources}
+        } tcp dport 9100 accept comment "Prometheus node exporter scrape"
+      ''
+    ]
+  );
 }

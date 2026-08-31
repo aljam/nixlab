1d1d7dec716369e944147ec23d803b5bdc7bd228
Aljam <aljam@live.ca>
2026-08-31 15:16:19 -0400

    Clean up media-node.nix by commenting unused imports
    Comment out unused feature imports for media node.

--- modules/roles/media-node.nix
+++ modules/roles/media-node.nix
@@ -3,18 +3,8 @@
 {
   imports = [
     ../features/homepage.nix
-    #../features/radarr.nix
-    #../features/sonarr.nix
-    #../features/lidarr.nix
-    #../features/bazarr.nix
-    #../features/seerr.nix
-    #../features/audiobookshelf.nix
     ../features/autobrr.nix
-    #../features/shoko.nix
-    #../features/torrents.nix
-    #../features/jellyfin.nix
     ../features/recyclarr.nix
-    #../features/prowlarr.nix
     ../features/vaultwarden.nix
     ../features/grafana.nix
     ../features/youtube.nix

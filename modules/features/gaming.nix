3ce5bc5bbff8d006ebfff3ff432102361f9a5260
Aljam <aljam@live.ca>
2026-08-31 12:41:57 -0400

    Remove custom Steam package configuration
    Removed custom package definition for Steam in gaming.nix.

--- modules/features/gaming.nix
+++ modules/features/gaming.nix
@@ -8,30 +8,10 @@
   programs.gamemode = {
     enable = true;
   };
+
   programs.gamescope.enable = true;
   programs.steam = {
     enable = true;
-    package =
-      let
-        millenniumPkgs = pkgs.appendOverlays [
-          inputs.millennium.overlays.default
-        ];
-      in
-      millenniumPkgs.millennium-steam.override {
-        extraPkgs = (pkgs: with pkgs; [
-          gamemode
-          libxi
-          libxcursor
-          libxinerama
-          libxscrnsaver
-          libpulseaudio
-          libvorbis
-          libkrb5
-          keyutils
-          at-spi2-atk
-          libpng
-        ]);
-      };
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
